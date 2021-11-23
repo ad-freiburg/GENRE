559e6a50a2c0a7550b46a4afcc51408e7e4ae076
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2021-11-23 15:52:46 +0100

    create trie with article titles of entities with relevant types only

--- get_trie.py
+++ get_trie.py
@@ -1,3 +1,6 @@
+import argparse
+from typing import Optional, Set, List
+
 import pickle
 
 from genre.fairseq_model import GENRE
@@ -5,20 +8,39 @@ from genre.trie import Trie
 from urllib.parse import unquote
 
 
-def get_wikipedia_articles_trie(model):
+def get_relevant_entity_qids() -> Set[str]:
+    qids = set()
+    for line in open("data/qid_to_relevant_types.tsv"):
+        qid = line.split("\t")[0]
+        qids.add(qid)
+        if len(qids) % 1000 == 0:
+            print(f"\r{len(qids)} QIDs", end="")
+    return qids
+
+
+def get_wikipedia_article_titles(relevant_qids: Optional[Set[str]] = None):
     url_prefix = "https://en.wikipedia.org/wiki/"
     titles = []
     print("read lines...")
     for line in open("data/qid_to_wikipedia_url.tsv"):
-        url = line[:-1].split("\t")[1]
+        values = line[:-1].split("\t")
+        if relevant_qids:
+            qid = values[0]
+            if qid not in relevant_qids:
+                continue
+        url = values[1]
         title = url[len(url_prefix):]
         title = unquote(title)
         title = title.replace("_", " ")
         titles.append(title)
+    return titles
+
+
+def get_trie(model, article_titles: List[str]) -> Trie:
     trie = Trie()
-    for i, title in enumerate(titles + ["NIL"]):
+    for i, title in enumerate(article_titles + ["NIL"]):
         if i % 1000 == 0:
-            print(f"\r{i} entities", end = "")
+            print(f"\r{i} entities", end="")
         encoded = model.encode(" }} [ {} ]".format(title))[1:].tolist()
         trie.add(encoded)
     return trie
@@ -30,8 +52,23 @@ def load_trie():
     return trie
 
 
-if __name__ == "__main__":
+def main(args):
+    if args.types:
+        print("read relevant entity qids...")
+        relevant_qids = get_relevant_entity_qids()
+        out_file = "data/entity_trie.relevant_types.pkl"
+    else:
+        relevant_qids = None
+        out_file = "data/entity_trie.tmp.pkl"
+    titles = get_wikipedia_article_titles(relevant_qids)
     model = GENRE.from_pretrained("models/fairseq_e2e_entity_linking_wiki_abs").eval()
-    trie = get_wikipedia_articles_trie(model)
-    with open("data/entity_trie.pkl", "wb") as f:
+    trie = get_trie(model, titles)
+    with open(out_file, "wb") as f:
         pickle.dump(trie, f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--types", action="store_true")
+    args = parser.parse_args()
+    main(args)
